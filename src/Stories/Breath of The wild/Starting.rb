e5af28ea3782de9b042c523631ae0977094869be
OrangeKami <chen.zhang.62@hotamil.com>
2022-04-18 16:20:59 +1000

    start @exp if/else

--- src/Stories/Breath of The wild/Starting.rb
+++ src/Stories/Breath of The wild/Starting.rb
@@ -1,6 +1,7 @@
 require_relative "../../book_layout.rb"
 require './Character.rb'
-require 'bundler'
+require 'colorize'
+require "bundler"
 Bundler.require(:default)
 
 class Starting 
@@ -37,12 +38,34 @@ class Starting
             menu.choice"Climb down to the forest"
             menu.choice"go to the path"
             end
+            line_break
             case path 
             when "Climb down to the forest"
                 chat "you fell down at half way"
+                # @exp = Character.exp_gain(50)
                 line_break_space
                 the_end
-            else
+                sleep 2
+                return start_story
+            when  "go to the path"
+                chat "You walk on the path and you see a wooden house."
+                line_break_space
+                chat "Please choose wooden house or path?"
+                line_break
+                choose1 = gets.chomp.downcase
+                if choose1.include? "w"
+                    chat "You are close to the wooden house and an older man shows up"
+                    line_break_space
+                    chat "He tells you there is a secret path will lead you to the castle in the forest and is an " + "empty ".cyan + "place."
+                    line_break_space
+                    chat "You leave the house and follow the path"
+                    # @exp = Character.exp_gain(500)
+                elsif choose1.include?
+                    chat "You are walking on the path with nice scene."
+                else
+                    chat "You have to choose your way"
+                   
+                end
             end
       end       
 end
